8daf3b1e85af6f6a1914705f598200a61b82ba79
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2021-01-23 12:36:57 +0200

    Delete effects.js

--- scripts/powers/effects.js
+++
@@ -1,8 +0,0 @@
-//flare bullet aoe
-const flareAoeEffect = newEffect(20, e => {
-    Draw.color(Color.white, Color.red, e.fin()); //color goes from white to red
-    Lines.stroke(e.fout() * 5); //line thickness goes from 5 to 0
-    Lines.circle(e.x, e.y, e.fin() * 10); //draw a circle whose radius goes from 0 to 10
-});
-
-UnitTypes.flare.weapons.get(0).bullet.hitEffect = flareAoeEffect;
