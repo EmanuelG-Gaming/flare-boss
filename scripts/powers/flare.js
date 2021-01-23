a4e27eedf09c90b7786285d625082ed7ffbfe756
EmanuelG-Gaming <68462759+EmanuelG-Gaming@users.noreply.github.com>
2021-01-23 14:08:45 +0200

    Update flare.js

--- scripts/powers/flare.js
+++ scripts/powers/flare.js
@@ -29,3 +29,12 @@ const flareAoeEffect = new Effect(20, e => {
 });
 
 UnitTypes.flare.weapons.get(0).bullet.hitEffect = flareAoeEffect;
+
+//flare shoot aoe
+const flareShootEffect = new Effect(15, e => {
+  Draw.color(Color.red, Color.white, e.fin()); //color goes from red to white
+  Lines.stroke(e.fout() * 2); //line thickness goes from 2 to 0
+  Lines.circle(e.x, e.y, e.fin() * 3); //draw a circle whose radius goes from 0 to 3
+});
+
+UnitTypes.flare.weapons.get(0).bullet.shootEffect = flareShootEffect;
